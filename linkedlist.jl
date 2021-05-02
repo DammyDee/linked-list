e1cb8c93c05c22a26dbbe3056abb1ae7c8be18a7
DammyDee <64790542+DammyDee@users.noreply.github.com>
2021-05-02 23:20:07 +0100

    Update linkedlist.jl

--- linkedlist.jl
+++ linkedlist.jl
@@ -1,13 +1,27 @@
 mutable struct Node # Define Node structure
     num :: Int
-    next :: Union{Node, Nothing}# Ensure next field of Node accepts a null value (nothing) or another Node
-end
-    
+    next :: Ref{Union{Node, Nothing}} # Create a Reference object that refers to a Node or to nothing (equivalent to NULL). 
+end                                   # Ref() sort of acts as a pointer
 
-n = Node(5, nothing)
-m = Node(7, nothing)
-t = Node(4, nothing)
-n. next = m
-m.next = t
-linked_list = n
-print(n)
+linked_list = Ref{Union{Nothing, Node}}(nothing)
+
+n = Node(5, Ref{Union{Nothing, Node}(nothing))
+m = Node(7, Ref{Union{Nothing, Node}(nothing))
+t = Node(4, Ref{Union{Nothing, Node}(nothing))
+
+linked_list[] = n
+print(linked_list)
+
+linked_list[].next[] = m
+
+print(linked_list)
+
+l = linked_list
+
+while !isnothing(l[])
+    l[] = l[].next[]
+end
+
+l[].next[] = t
+
+print(linked_list)
